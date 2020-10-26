f5adbd6d6e9d1f05e5bbe4f72be75a6604e78406
Konstantin Bukin <bukin@inetstudio.ru>
2020-10-26 09:07:48 +0300

    Update framework

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => 'InetStudio\ACL\Users\Models\UserModel',
+            'model' => InetStudio\ACL\Users\Models\UserModel::class,
         ],
 
         // 'users' => [
